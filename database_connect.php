207ff3a8d4a6594ef90f7da196dc93a395c323cf
ABHISHEK Joshi <abhijoshi2k@gmail.com>
2020-11-29 17:08:01 +0530

    Update database_connect.php

--- database_connect.php
+++ database_connect.php
@@ -1,10 +1,19 @@
 <?php
 
-$mysql_host='localhost';
-$mysql_user='root';
-$mysql_pass='pass123';
 
-$mysql_db='pizza';
+// $mysql_host='localhost';
+// $mysql_user='root';
+// $mysql_pass='pass123';
+// $mysql_db='pizza';
+
+//awardspace db_name: 3667817_pizza
+//awardspace db_password: PfgsohjN7R3GD(36
+
+$mysql_host='fdb29.awardspace.net';
+$mysql_user='3667817_pizza';
+$mysql_pass='PfgsohjN7R3GD(36';
+
+$mysql_db='3667817_pizza';
 
 $connect=@mysqli_connect($mysql_host,$mysql_user,$mysql_pass,$mysql_db);
 if(!$connect)
